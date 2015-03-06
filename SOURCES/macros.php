58d80bf6b655ae192cd4448caba236e8d07ceab2
Kurt Newman <kurt.newman@cpanel.net>
2015-03-06 14:23:19 -0600

    Update for ea namespace, patch agreement, and 5.6.6
    EA-387: This contains several changes.
      1. Updated Makefile to accomodate local and server
         builds using OBS tools.  This also removes Mock
         usage.
      2. Added PHP 5.6.6
      3. Updated patches to match team agreement:
           https://cpanel.wiki/display/EA/PHP+Patches
      4. Disabled the user of system libzip, and now uses
         the version that comes with PHP.  This ensures
         we don't have to deploy a dependency because
         the CentOS 7 version is < 0.11; which is required
         by PHP.
      5. Building with dtrace support on CentOS 7 requires
         python.  Added this dependency to the spec file.

---
+++ SOURCES/macros.php
@@ -0,0 +1,15 @@
+#
+# Interface versions exposed by PHP:
+# 
+%php56_php_core_api @PHP_APIVER@
+%php56_php_zend_api @PHP_ZENDVER@
+%php56_php_pdo_api  @PHP_PDOVER@
+%php56_php_version  @PHP_VERSION@
+
+%php56_php_extdir   @LIBDIR@/php/modules
+
+%php56_php_inidir   @ETCDIR@/php.d
+
+%php56_php_incldir  @INCDIR@/php
+
+%php56___php        @BINDIR@/php
